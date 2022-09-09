8496bdbc215f3ee8ace6d10a41703e23b3f3a87c
Saurabh <61317144+html-css-js-art@users.noreply.github.com>
2022-09-09 12:59:50 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Alert from 'react-bootstrap/Alert';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+ReactDOM.render(
   <React.StrictMode>
+    <>
+    <Alert variant="success">
+        <Alert.Heading>Hey! Welcome</Alert.Heading>
+        <p>
+          This is a simple Todo List App. You can add your todos here. You can also delete them. This app is made using ReactJS And Bootstrap.
+        </p>
+        <hr />
+        <div className="d-flex justify-content-end">
+        </div>
+      </Alert>
+    </>
     <App />
-  </React.StrictMode>
+    
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
